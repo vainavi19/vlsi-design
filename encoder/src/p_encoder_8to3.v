99033a9c35bb34f6c8e33b73359fc86080ea0829
vainavi ankam <vainavi.ankam@gmail.com>
2025-05-26 11:32:30 +0530

    Update and rename encoder_8to3.v to p_encoder_8to3.v

--- encoder/src/p_encoder_8to3.v
+++ encoder/src/p_encoder_8to3.v
@@ -18,10 +18,9 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-//PRIORITY 8-TO-3 ENCODER USING 4-TO-2 ENCODERS
-//OCTAL TO BINARY CONVERTER 
-module encoder_8to3(
+//8-to-3 encooder using two 4-to-2 encoders
+//also acts as octal to binary code converter 
+module p_encoder_8to3(
 input wire [7:0] in,
 output wire [2:0] e,
 output wire v
@@ -29,7 +28,6 @@ output wire v
 wire [2:0] eu,ed;
 wire vu,vd;
 
-// Instantiate the two 4-to-2 encoders
 p_encoder_4to2 up (
 .in(in[7:4]),
 .e(eu),
@@ -42,14 +40,14 @@ p_encoder_4to2 down (
 .v(vd)
 );
 
-// dataflow
+// dataflow - combinational ckt after 4-to-2 encoders
 assign v = vu | vd;
 
 assign e[2] = vd;  // Indicates which half of the 8 inputs is active
 assign e[1] = (eu[1] & ~vd) | (ed[1] & vd);
 assign e[0] = (eu[0] & ~vd) | (ed[0] & vd);
 
-//gate level
+//gate level - combinational ckt after 4-to-2 encoders
 wire w1,w2,w3,w4;
 or(v, vu,vd);
 
@@ -64,7 +62,6 @@ and(e[2], 1,vd);
 endmodule
 
 
-
 //*****************************************************************
 module p_encoder_4to2(
 input wire [3:0] in,
@@ -72,12 +69,12 @@ output wire [1:0] e,
 output wire v
 );
 
-/*
+
 //dataflow
 assign e[0] = (in[1] & ~in[0]) | in[0];
 assign e[1] = (in[2] & ~in[1] & ~in[0]) | in[0];
 assign v = in[0] | in[1] | in[2] | in[3];  // v is high if any input is high
-*/
+
 
 
 //gate level
@@ -93,3 +90,47 @@ or(e[0],w4,in[0]);
 or(v,in[0],in[1],in[2],in[3]);
 
 endmodule
+
+
+/////////////////////////////////////////////////////
+//8-to-3 encooder - behavioral modelling 
+module p_encoder_8to3 (
+input [7:0] in,
+output reg [2:0] e
+);
+
+always @(in) begin
+casex(in)
+8'b00000001: e=3'b000;
+8'b0000001x: e=3'b001;
+8'b000001xx: e=3'b010;
+8'b00001xxx: e=3'b011;
+8'b0001xxxx: e=3'b100;
+8'b001xxxxx: e=3'b101; 
+8'b01xxxxxx: e=3'b110; 
+8'b1xxxxxxx: e=3'b111; 
+default: e=3'bx;
+endcase 
+end 
+endmodule
+
+
+/////////////////////////////////////////////////////
+//8-to-3 encooder - dataflow modelling 
+module p_encoder_8to3(
+input [7:0] in,
+output [2:0] e
+);
+
+assign e = (in[7] ==1'b1 ) ? 3'b111:
+(in[6] ==1'b1 ) ? 3'b110:
+(in[5] ==1'b1 ) ? 3'b101:
+(in[4] ==1'b1) ? 3'b100:
+(in[3] ==1'b1) ? 3'b011:
+(in[2] ==1'b1) ? 3'b010:
+(in[1] ==1'b1) ? 3'b001:
+(in[0] ==1'b1) ? 3'b000: 3'bxxx;
+endmodule
+/////////////////////////////////////////////////////
+//8-to-3 encooder - gate level modelling 
+  
